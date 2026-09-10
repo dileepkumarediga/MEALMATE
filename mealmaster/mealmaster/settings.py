24a542574d580d4acd004b0aae5396856478c346
Dileep Kumar <dileeparya3138@gmail.com>
2026-09-10 21:18:38 +0530

    Update settings.py

--- mealmaster/mealmaster/settings.py
+++ mealmaster/mealmaster/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mealmaster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'DIRS': [BASE_DIR / 'delivery' / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
